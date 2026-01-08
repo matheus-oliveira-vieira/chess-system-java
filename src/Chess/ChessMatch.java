51c3a137241de289143aba4029253ea33c990b8b
Matheus Oliveira <matheus.oliveirav@gmail.com>
2026-01-08 15:20:24 -0300

    Create exception BoardException

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -25,12 +25,7 @@ public class ChessMatch {
 
   private void initialSetup() {
     board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-    // board.placePiece(new Rook(board, Color.WHITE), new Position('h', 1));
     board.placePiece(new King(board, Color.BLACK), new Position(0 , 4));
     board.placePiece(new King(board, Color.WHITE), new Position(7 , 4));
-
-    // board.placePiece(new Rook(board, Color.BLACK), new ChessPosition('a', 8).toPosition());
-    // board.placePiece(new Rook(board, Color.BLACK), new ChessPosition('h', 8).toPosition());
-    // board.placePiece(new King(board, Color.BLACK), new ChessPosition('e', 8).toPosition());
   }
 }
