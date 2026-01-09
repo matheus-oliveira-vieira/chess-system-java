f03e3cb00c6fee64ae7b6d53807fce57a988d490
Matheus Oliveira <matheus.oliveirav@gmail.com>
2026-01-09 14:37:21 -0300

    Add checkmate logic

--- src/application/App.java
+++ src/application/App.java
@@ -15,7 +15,7 @@ public class App {
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
-        while (true) {
+        while (!chessMatch.getCheckMate()) {
             try {
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
@@ -42,5 +42,7 @@ public class App {
                 sc.nextLine();
             }
         }
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
