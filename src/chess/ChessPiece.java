9463f8e0ff373d6353742835acb733de29e8c2fe
Matheus Oliveira <matheus.oliveirav@gmail.com>
2026-01-12 09:20:26 -0300

    add pawn logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -11,6 +11,7 @@ public abstract class ChessPiece extends Piece{
   public ChessPiece(Board board, Color color) {
     super(board);
     this.color = color;
+    this.moveCount = 0;
   }
 
   public Color getColor() {
